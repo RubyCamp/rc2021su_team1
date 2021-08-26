59954a56cb5c2d9ebc30d10bbc05f93938a74034
inoue-ayumu <Noizeria@outlook.jp>
2021-08-26 15:52:49 +0900

    'SPACEをSpaceにした'

--- title/director.rb
+++ title/director.rb
@@ -7,7 +7,7 @@ module Title
         def play
             Window.draw(0, 0, @bg)
             Window.draw_font(150, 170, "Invite visitors to Shimane", Font.new(48),:color => C_RED)
-            Window.draw_font(430, 550, "Push SPACE key to start ", Font.new(32),:color => C_BLUE)
+            Window.draw_font(430, 550, "Push Space key to start ", Font.new(32),:color => C_BLUE)
         end
     end
 
